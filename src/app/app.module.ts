0e9e4cb4fc276fb4a8fc2ee319e8d610ad879dee
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-02-19 15:29:33 -0300

    import CursosModule, CursosComponent, CursoDetalheComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
+import { CursosModule } from './cursos/cursos/cursos.module';
+import { CursosComponent } from './cursos/cursos/cursos.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CursoDetalheComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]

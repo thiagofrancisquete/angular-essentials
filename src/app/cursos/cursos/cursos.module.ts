a14b804c54bf19590a4bae9b7b5e74c7d787c023
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-02-19 15:29:47 -0300

    import app-cursos template

---
+++ src/app/cursos/cursos/cursos.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import  { CursosComponent } from './cursos.component'
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    CursosComponent
+  ],
+  exports: [
+    CursosComponent
+  ]
+})
+export class CursosModule { }

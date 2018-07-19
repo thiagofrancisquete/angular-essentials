798811055f14654d2d546feff25fa1ad860fe401
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-07-19 16:50:44 -0300

    criando componentes manualmente e pelo CLI

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-primeiro-component',
+  template: `
+    <p>Meu primeiro component</p>
+  `
+})
+export class MeuPrimeiroComponent { }

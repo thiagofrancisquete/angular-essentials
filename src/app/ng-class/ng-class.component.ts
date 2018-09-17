c2416db642c86cdcf9bf976ac685e337c64f8d65
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-09-17 18:09:15 -0300

    ngClass

---
+++ src/app/ng-class/ng-class.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-class',
+  templateUrl: './ng-class.component.html',
+  styleUrls: ['./ng-class.component.css']
+})
+export class NgClassComponent implements OnInit {
+  meuFavorito: boolean = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onClick() {
+   this.meuFavorito = !this.meuFavorito;
+  }
+}

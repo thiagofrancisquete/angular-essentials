9f838056f910dbb59bc04e0795dc2abe42dc744f
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-10-13 18:21:09 -0300

    criando a diretiva ngElse

---
+++ src/app/diretivas/ng-else.directive.ts
@@ -0,0 +1,17 @@
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[ngElse]'
+})
+export class NgElseDirective {
+  
+  @Input() set ngElse(condition: boolean) {
+    if(!condition){
+      this.viewRef.createEmbeddedView(this.templateRef);
+    } else {
+      this.viewRef.clear();
+    }
+  }
+  constructor(private templateRef: TemplateRef<any>, private viewRef: ViewContainerRef) { }
+
+}

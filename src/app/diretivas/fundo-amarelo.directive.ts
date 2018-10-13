e18394d21c1d618090406b924dbf9e02e56526b0
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-10-13 15:19:55 -0300

    diretiva customizada usando Renderer2 e ElementRef

--- src/app/diretivas/fundo-amarelo.directive.ts
+++ src/app/diretivas/fundo-amarelo.directive.ts
@@ -1,15 +1,15 @@
 import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[fundoAmarelo]'
+  selector: 'p[fundoAmarelo]'
 })
 export class FundoAmareloDirective {
-
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
-    // this.elementRef.nativeElement.style.backgroundColor = 'yellow'; <- vulneravel
-    this.renderer.setStyle(this.elementRef.nativeElement, 
-      'background-color', 'yellow'  
+  constructor(private _elementRef: ElementRef, private _renderer: Renderer2) {
+    // this._elementRef.nativeElement.style.backgroundColor = 'yellow';
+    this._renderer.setStyle(
+      this._elementRef.nativeElement,
+      'backgroundColor',
+      'yellow'
     );
-   }
-
+  }
 }

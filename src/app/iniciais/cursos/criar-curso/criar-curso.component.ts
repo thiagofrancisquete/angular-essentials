0a52965eb828da7c950378d54b184408ba1215c4
thiagofrancisquete <thiago.francisquete@gmail.com>
2019-07-19 17:36:28 -0300

    criado metodo onAddCurso

--- src/app/iniciais/cursos/criar-curso/criar-curso.component.ts
+++ src/app/iniciais/cursos/criar-curso/criar-curso.component.ts
@@ -1,3 +1,4 @@
+import { CursosService } from './../cursos.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,8 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CriarCursoComponent implements OnInit {
 
+  cursos: string[];
+
+  constructor(private cursosService: CursosService) {
+    this.cursos = this.cursosService.getCursos();
+  }
 
   ngOnInit() {
-    console.log('criar curso carregado');
+    console.log('criar curso  carregado');
+  }
+
+  onAddCurso(curso: string) {
+    this.cursosService.addCurso(curso);
   }
 }

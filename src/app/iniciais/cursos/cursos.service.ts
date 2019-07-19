08aa00f528e7f5c4bcde59f5063a78f62ad5950d
thiagofrancisquete <thiago.francisquete@gmail.com>
2019-07-19 17:35:56 -0300

    criado metodo addCurso

--- src/app/iniciais/cursos/cursos.service.ts
+++ src/app/iniciais/cursos/cursos.service.ts
@@ -1,11 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 
 @Injectable()
 export class CursosService {
 
+  emitirCursoCriado = new EventEmitter<string>();
+
+  private cursos: string[] = ['Java', 'Ext Js', 'Angular'];
+
   constructor() { }
 
   getCursos() {
-    return ['Java', 'Ext Js', 'Angular'];
+    return this.cursos;
+  }
+
+  addCurso(curso: string) {
+    this.cursos.push(curso);
+    this.emitirCursoCriado.emit(curso);
   }
 }
